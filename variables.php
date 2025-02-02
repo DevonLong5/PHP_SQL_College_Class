d4190b7e94aac34049e95662325ce590ec3be6f2
DevonLong5 <devonlong04@gmail.com>
2025-02-02 14:44:51 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php
+$first_name = "Devon";
+$last_name = "Long";
+$address = "123 N Dakota Dr";
+$phone_number = "505-243-2034";
+
+print "<p>$first_name $last_name lives on $address and his phone number is $phone_number.</p>";
+
+?>
+</pre>
+</body>
+</html>
